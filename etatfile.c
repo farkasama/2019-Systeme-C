8abe01267b5e746aff3fd93f5e2e4c71015a2a31
Etienne Jouanne <etienne.jouanne@gmail.com>
2019-05-07 17:17:58 +0200

    Ajout commentaires

--- etatfile.c
+++ etatfile.c
@@ -1,49 +1,54 @@
-//
-// Created by will on 23/04/19.
-//
 #include "STRUCTURE.c"
 
 size_t msg_message_size(MESSAGE *m) {
+
+    //Si la file est inutilisable
     if (m == NULL) {
         perror("message inutilisable");
         return -1;
     }
+
+    //Si la memoire partager est inutilisable
     if (m->mp == NULL) {
         perror("memoire partagée inutilisable");
         return -1;
     }
+
+    //retourne la longueur
     return m->mp->longueur;
 }
 
 size_t msg_capacite(MESSAGE *m) {
-    if (m == NULL) {
-        perror("message inutillisable ");
-        return -1;
-    }
-    if (m->mp == NULL) {
-        perror("memoire partagée inutilisable");
-        return -1;
-    }
-    return m->mp->capacite;
-}
 
-size_t msg_nb(MESSAGE *m) {
+    //Si la file est inutilisable
     if (m == NULL) {
         perror("message inutilisable");
         return -1;
     }
+
+    //Si la memoire partager est inutilisable
     if (m->mp == NULL) {
-        perror("message partagée inutlisable");
+        perror("memoire partagée inutilisable");
         return -1;
     }
-    if (m->mp->first == -1) {
-        return 0;
+
+    //retourne le nombre maximal
+    return m->mp->nb_message_max;
+}
+
+size_t msg_nb(MESSAGE *m) {
+    //Si la file est inutilisable
+    if (m == NULL) {
+        perror("message inutilisable");
+        return -1;
     }
-    if (m->mp->first == m->mp->last) {
-        return m->mp->capacite;
+
+    //Si la memoire partager est inutilisable
+    if (m->mp == NULL) {
+        perror("memoire partagée inutilisable");
+        return -1;
     }
-    if (m->mp->first > m->mp->last) {
-        return m->mp->first - m->mp->last;
-    }
-    return m->mp->first - m->mp->last + m->mp->capacite;
+
+    //Retourne le nombre de message dans la file
+    return m->mp->nb_message;
 }
